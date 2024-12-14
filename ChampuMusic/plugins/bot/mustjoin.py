6ab831d60742eeaac981e236a57f968bb2d08260
Highpower143 <nandyadu1c@gmail.com>
2024-12-14 18:48:44 +0530

    Update mustjoin.py

--- ChampuMusic/plugins/bot/mustjoin.py
+++ ChampuMusic/plugins/bot/mustjoin.py
@@ -6,7 +6,7 @@ from ChampuMusic import app
 
 #--------------------------
 
-MUST_JOIN = "Starboy_HUBS"
+MUST_JOIN = "CreativeYdv"
 #------------------------
 @app.on_message(filters.incoming & filters.private, group=-1)
 async def must_join_channel(app: Client, msg: Message):
